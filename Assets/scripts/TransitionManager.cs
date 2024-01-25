9267d6b298e09bad00d8e37e32bb27e11abf6871
louis lasserre <louis.lasserre@mmibordeaux.com>
2024-01-25 09:22:06 +0100

    timeline and scene transition

--- Assets/scripts/TransitionManager.cs
+++ Assets/scripts/TransitionManager.cs
@@ -2,42 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TextCore;
+using UnityEngine.Playables;
 // using UnityEngine.UI;
 
 public class TransitionManager : MonoBehaviour
 {
     public FadeScreen fadeScreen;
-    public Animator sceneAnimation;
+    
+    public PlayableDirector currentTimeline;
+    
     public string levelToLoad;
-    private float timer = 8f;
-    // private Text timerSeconds;
+    private float timer = 2f;
+    //private Text timerSeconds;
+
+    private bool changeScene = false;
+    
+
+    
+    private double timelineTime;
 
     void Start()
     {
-        // timerSeconds = GetComponent<Text>();
+        //timerSeconds = GetComponent<Text>();
+        
     }
 
     void Update()
     {
-        //if (sceneAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        //{
-        //    timer -= Time.deltaTime;
-        //    // timerSeconds.text = timer.ToString("f2");
-        //    Debug.Log(timer);
-        //    if (timer <= fadeScreen.fadeDuration && timer > 0)
-        //    {
-        //        Debug.Log("fadout");
-        //        fadeScreen.FadeOut();
-        //    }
-        //    else if (timer <= 0)
-        //    {
-        //        Debug.Log("transition");
-        //        SceneManager.LoadScene(levelToLoad);
-        //    }
-        //}
-
-
         
+        timelineTime = currentTimeline.time;
+
+        Debug.Log(timelineTime);
+        Debug.Log(currentTimeline.duration);
+        
+        if (timelineTime >= currentTimeline.duration - 0.5f)
+        {
+            changeScene = true;
+            
+        }
+
+        if(changeScene == true)
+        {
+            timer -= Time.deltaTime;
+            //timerSeconds.text = timer.ToString("f2");
+            Debug.Log(timer);
+            if (timer <= fadeScreen.fadeDuration && timer > 0)
+            {
+                Debug.Log("fadout");
+                fadeScreen.FadeOut();
+            }
+            else if (timer <= 0)
+            {
+                Debug.Log("transition");
+                SceneManager.LoadScene(levelToLoad);
+            }
+        }
+
     }
     // public void GoToScene(int sceneIndex)
     // {
